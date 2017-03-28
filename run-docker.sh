081d3117c8cc2b806ad295599b3ee6cd4cfe947f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-28 03:39:20 +0200

    Clean up

--- run-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -e
-
-docker build -t lila-buildenv .
-docker run --volume /home/niklas/Projekte/lila:/home/builder/lila --user $(id -u) lila-buildenv
--- stage-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-echo "Compiling client ..."
-./bin/prod/compile-client
-
-export JAVA_OPTS="-Xms1024M -Xmx1024M -XX:ReservedCodeCacheSize=64m -XX:+UseConcMarkSweepGC"
-sbt ";stage;exit"
