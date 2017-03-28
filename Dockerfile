12d8e44afec2e29af50daa5418db08185145eb82
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-03-28 03:36:45 +0200

    Actually try build on stage

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,20 @@
 FROM debian:testing
 
-MAINTAINER Niklas Fiekas <niklas.fiekas@backscattering.de>
-
-RUN apt-get update && apt-get install -y --no-install-recommends git-core nodejs openjdk-8-jdk-headless apt-transport-https gnupg
-RUN apt-get install dirmngr
+RUN apt-get update && apt-get install -y --no-install-recommends git-core nodejs openjdk-8-jdk-headless apt-transport-https gnupg dirmngr npm nodejs-legacy
 
 RUN echo "deb https://dl.bintray.com/sbt/debian /" > /etc/apt/sources.list.d/sbt.list
 RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 RUN apt-get update && apt-get install -y sbt
 
-RUN apt-get install -y npm nodejs-legacy
-RUN npm install -g gulp-cli && npm install -g npm
+RUN npm install -g npm && npm install -g npm
+RUN npm install -g gulp-cli
 
 RUN useradd --create-home --user-group builder
 
-USER builder:builder
 VOLUME /home/builder/lila
 WORKDIR /home/builder/lila
 
+COPY stage-deploy.sh /home/builder/stage-deploy.sh
+COPY build-deps.sh /home/builder/build-deps.sh
 
-COPY stage-deploy.sh /home/builder
-COPY build-deps.sh /home/builder
-
-RUN /home/builder/build-deps.sh
-
-CMD ../stage-deploy.sh
+#RUN /home/builder/build-deps.sh
--- stagebot.py
+++ stagebot.py
@@ -4,8 +4,12 @@ import os
 import time
 import logging
 import subprocess
+import tempfile
 
 from slackclient import SlackClient
+
+def sh(args):
+    return subprocess.check_output(args, stderr=subprocess.STDOUT)
 
 class SlackBot:
     def __init__(self, token):
@@ -16,6 +20,9 @@ class SlackBot:
             raise Exception("rtm connect failed")
 
         logging.info("Connected.")
+
+        self.deploy()
+        return
 
         while True:
             for msg in self.slack.rtm_read():
@@ -29,15 +36,43 @@ class SlackBot:
         logging.info(message)
         self.slack.rtm_send_message("bot-testing", message)
 
+    def post_snippet(self, title, snippet):
+        self.slack.api_call("files.upload", channels="bot-testing",
+                            title=title, content=snippet or "<no output>", filetype="txt")
+
     def handle_message(self, message):
         if "deploy" in message and "stagebot" in message:
             self.deploy()
 
-    def deploy(self):
-        self.send("deploying ...")
-        self.send("deployed (or not)")
+    def deploy(self):	
+        try:
+            self.send("Building stagebot-buildenv ...")
+            sh(["docker", "build", "-t", "stagebot-buildenv", "."])
+
+            tmpdir = "/tmp/lila.tmp"
+            if tmpdir:
+            #with tempfile.TemporaryDirectory(prefix="stagebot") as tmpdir:
+                self.send("Cloning repository ...")
+                #sh(["git", "clone", "/home/stagebot/lila", "--recursive", "--shared", tmpdir])
+
+                DOCKER_RUN = ["docker", "run", "--volume", "%s:/home/builder/lila" % tmpdir, "stagebot-buildenv"]
+
+                #self.send("Compiling ...")
+                #sh(DOCKER_RUN + ["sbt", "stage"])
+
+                self.send("Building ui ...")
+                sh(DOCKER_RUN + ["./ui/build", "prod"])
+
+                self.send("Deploying assets ...")
+                sh(["rsync", "--archive", "--no-o", "--no-g", "%s/public" % tmpdir, "/home/lichess-stage"])
+                sh(["chown", "-R", "lichess:lichess", "/home/lichess-stage"])
+     
+            self.send("Done.")
+        except subprocess.CalledProcessError as err:
+            logging.exception("Deploy failed")
+            self.post_snippet(str(err), err.output.decode("utf-8", errors="ignore"))
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
     bot = SlackBot(os.environ["SLACK_BOT_TOKEN"])
     bot.run()
