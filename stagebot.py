c187acbaf1941df29c60297e8cb7c62930db984a
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-04-06 20:51:00 +0200

    download requested branch artifacts

--- stagebot.py
+++ stagebot.py
@@ -7,6 +7,8 @@ import subprocess
 import tempfile
 import configparser
 import argparse
+import sys
+import urllib.request as urllib
 
 from slackclient import SlackClient
 
@@ -14,13 +16,18 @@ def sh(args):
     return subprocess.check_output(args, stderr=subprocess.STDOUT)
 
 
-class ArgumentError(Exception):
+class ParserError(Exception):
     pass
 
+class ParserMessage(Exception):
+    pass
 
 class ArgumentParser(argparse.ArgumentParser):
     def error(self, message):
-        raise ArgumentError(message)
+        raise ParserError(message)
+
+    def _print_message(self, message, file=None):
+        raise ParserMessage(message)
 
 
 class SlackBot:
@@ -28,26 +35,31 @@ class SlackBot:
         self.config = configparser.ConfigParser()
         self.config.read("stagebot.ini")
 
-        self.slack = SlackClient(self.config.get("stagebot", "slack_bot_token"))
 
         self.parser = ArgumentParser()
-        self.parser.add_argument("command", choices=["echo"])
+        self.parser.add_argument("command", choices=["echo", "deploy"])
+        self.parser.add_argument("--branch", default="master")
 
-    def run(self):
+        self.slack = SlackClient(self.config.get("slack", "bot_token"))
+        self._connect()
+
+    def _connect(self):
         if not self.slack.rtm_connect():
             raise Exception("rtm connect failed")
 
         logging.info("Connected.")
 
+    def run(self):
         while True:
             for msg in self.slack.rtm_read():
                 logging.debug("%s", msg)
+
                 if msg.get("type") == "message":
-                    try:
-                        self.handle_message(msg["text"])
-                    except subprocess.CalledProcessError as err:
-                        logging.exception("Command failed")
-                        self.post_snippet(str(err), err.output.decode("utf-8", errors="ignore"))
+                    args = msg["text"].split()
+                    if args[0].lower() not in ["stagebot", "@stagebot"]:
+                        continue
+
+                    self.handle(args[1:])
 
             time.sleep(1)
 
@@ -59,21 +71,34 @@ class SlackBot:
         self.slack.api_call("files.upload", channels="bot-testing",
                             title=title, content=snippet or "<no output>", filetype="txt")
 
-    def handle_message(self, message):
-        call = message.split()
-        if call[0].lower() not in ["stagebot", "@stagebot"]:
-            return
-
+    def handle(self, args):
         try:
-            args = self.parser.parse_args(call[1:])
-        except ArgumentError as err:
+            args = self.parser.parse_args(args)
+        except ParserMessage as msg:
+            self.send("%s" % msg)
+            return
+        except ParserError as err:
             self.send(":interrobang: %s" % err)
             return
 
         if args.command == "echo":
             self.send("echo")
+        elif args.command == "deploy":
+            self.deploy(args)
+
+    def deploy(self, args):
+        branch = "build-artifacts"
+        download = "%s/lila-%s.tar.gz" % (self.config.get("s3", "bucket"), branch)
+        self.send("Downloading %s ..." % download)
+        urllib.urlretrieve(download, "lila.tar.gz")
+
+        self.send("Deploying ...")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    SlackBot().run()
+    bot = SlackBot()
+    if len(sys.argv) > 1:
+        bot.handle(sys.argv[1:])
+    else:
+        bot.run()
