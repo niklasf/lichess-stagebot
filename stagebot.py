17b065473721397db6fadc1237c705124079145c
Niklas Fiekas <niklas.fiekas@backscattering.de>
2017-04-06 20:19:39 +0200

    handle argument errors

--- stagebot.py
+++ stagebot.py
@@ -13,6 +13,16 @@ from slackclient import SlackClient
 def sh(args):
     return subprocess.check_output(args, stderr=subprocess.STDOUT)
 
+
+class ArgumentError(Exception):
+    pass
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        raise ArgumentError(message)
+
+
 class SlackBot:
     def __init__(self):
         self.config = configparser.ConfigParser()
@@ -20,7 +30,7 @@ class SlackBot:
 
         self.slack = SlackClient(self.config.get("stagebot", "slack_bot_token"))
 
-        self.parser = argparse.ArgumentParser()
+        self.parser = ArgumentParser()
         self.parser.add_argument("command", choices=["echo"])
 
     def run(self):
@@ -50,7 +60,16 @@ class SlackBot:
                             title=title, content=snippet or "<no output>", filetype="txt")
 
     def handle_message(self, message):
-        args = self.parser.parse_args(message.split())
+        call = message.split()
+        if call[0].lower() not in ["stagebot", "@stagebot"]:
+            return
+
+        try:
+            args = self.parser.parse_args(call[1:])
+        except ArgumentError as err:
+            self.send(":interrobang: %s" % err)
+            return
+
         if args.command == "echo":
             self.send("echo")
 
